77b02e69cdb0ccf4c67892bbcbe55c190c8b06fc
Dan Green <danngreen1@gmail.com>
2026-03-13 20:46:52 -0700

    Add level titles between levels

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var start_level: PackedScene
 
 var current_level: Node
+var level_num := 0
 
 
 func _ready() -> void:
@@ -10,6 +11,11 @@ func _ready() -> void:
 	current_level = get_node_or_null("Level")
 	if start_level:
 		change_level(start_level)
+	# Hide player until intro is dismissed
+	var player = $Player
+	player.respawning = true
+	player.get_node("CollisionShape2D").disabled = true
+	player.get_node("Sprite").visible = false
 	_show_intro()
 
 
@@ -41,6 +47,39 @@ func change_level(level_scene: PackedScene) -> void:
 	player.velocity = Vector2.ZERO
 	player.spawn_position = player.global_position
 
+	level_num += 1
+	_show_level_title()
+
+
+func _show_level_title() -> void:
+	var layer = CanvasLayer.new()
+	layer.layer = 15
+	add_child(layer)
+
+	var label = Label.new()
+	var font = load("res://assets/ShareTechMono-Regular.ttf")
+	label.text = "LEVEL " + str(level_num)
+	label.add_theme_font_override("font", font)
+	label.add_theme_font_size_override("font_size", 64)
+	label.add_theme_color_override("font_color", Color.WHITE)
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.position = Vector2(0, 300)
+	label.size = Vector2(1280, 80)
+	label.modulate.a = 0.0
+	layer.add_child(label)
+
+	var tween = create_tween()
+	tween.tween_property(label, "modulate:a", 1.0, 0.3)
+	tween.tween_interval(1.5)
+	tween.tween_property(label, "modulate:a", 0.0, 0.5)
+	tween.tween_callback(layer.queue_free)
+
+
+func initial_spawn() -> void:
+	var player = $Player
+	player.global_position.y = player.spawn_position.y - 25
+	player._begin_body_grow()
+
 
 func start_guide() -> void:
 	var guide = preload("res://scenes/ui/guide.tscn").instantiate()
@@ -62,4 +101,5 @@ func _finish_game_over() -> void:
 	player.get_node("Sprite/ColorRect").visible = true
 	player.get_node("Sprite").pivot_offset = Vector2(14, 24)
 	player._update_hud()
+	level_num = 0
 	change_level(start_level)
